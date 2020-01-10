1e36825f75ad36ea82909d6916e31c16a5d2167c
Naaman <naaman.the.dev@gmail.com>
2020-01-10 22:06:07 +0000

    feat(are-there-duplicates): implement algorithm for checking if there are duplicates in a collection

--- problem_solving_patterns/src/frequency_counter_pattern/mod.rs
+++ problem_solving_patterns/src/frequency_counter_pattern/mod.rs
@@ -1,7 +1,8 @@
-pub mod same_digit_frequency;
 /// # Frequency Counter Pattern
 ///
 /// Uses some form of constant time lookup data structure to store frequencies of elements
 /// in an iterable.
 /// This allows for frequency counting in O(n) time as opposed to requiring nested loops.
+pub mod are_there_duplicates;
+pub mod same_digit_frequency;
 pub mod valid_anagram_challenge;
