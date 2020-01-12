05fdacfb106fd0c37a2dda391be93b29c9e9c92a
Naaman <naaman.the.dev@gmail.com>
2020-01-12 13:52:31 +0000

    feat(min-sub-array-length): implement algorithm for the minimum sub array that exceeds a total

--- problem_solving_patterns/src/sliding_window/mod.rs
+++ problem_solving_patterns/src/sliding_window/mod.rs
@@ -3,3 +3,4 @@
 /// This uses a "Window" which slides along to look at sub-sections of a
 /// collection without needing to calculate the whole sub-section each time
 pub mod max_array_sum;
+pub mod min_sub_array_length;
