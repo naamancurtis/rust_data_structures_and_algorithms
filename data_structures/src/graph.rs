a92929e8006547b3a6d6ab38ed215e418cef74c7
Naaman Curtis <naaman.the.dev@gmail.com>
2020-02-26 12:41:29 +0000

    feat(undirected-graph): implement can traverse to and traverse all

--- data_structures/src/graph.rs
+++ data_structures/src/graph.rs
@@ -1,5 +1,7 @@
+use crate::deque;
+use crate::singly_linked_list::SinglyLinkedList;
 use std::collections::hash_map::DefaultHasher;
-use std::collections::HashMap;
+use std::collections::{HashMap, HashSet};
 use std::hash::{Hash, Hasher};
 
 #[derive(Default)]
@@ -51,22 +53,22 @@ where
         self.key_value_map.entry(key).or_insert(node);
     }
 
-    pub fn remove_vertex(&mut self, key: &'a T) -> Option<&T> {
+    pub fn remove_vertex(&mut self, key: &T) -> Option<&T> {
         let key = get_key(key);
         let relations = self.adjacency_list.remove(&key);
 
-        relations.map(|relations| {
+        if let Some(relations) = relations {
             relations.iter().for_each(|relation_key| {
-                self.adjacency_list.get_mut(relation_key).map(|relation| {
+                if let Some(relation) = self.adjacency_list.get_mut(relation_key) {
                     relation.retain(|relation_key| *relation_key != key);
-                });
+                }
             })
-        });
+        }
 
         self.key_value_map.remove(&key).map(|node| node.value)
     }
 
-    pub fn add_edge(&mut self, value_1: &'a T, value_2: &'a T) {
+    pub fn add_edge(&mut self, value_1: &T, value_2: &T) {
         let key1 = get_key(value_1);
         let key2 = get_key(value_2);
 
@@ -82,20 +84,20 @@ where
         let key_1 = get_key(value_1);
         let key_2 = get_key(value_2);
 
-        self.adjacency_list
-            .get_mut(&key_1)
-            .map(|relations| relations.retain(|relation_key| *relation_key != key_2));
+        if let Some(relations) = self.adjacency_list.get_mut(&key_1) {
+            relations.retain(|relation_key| *relation_key != key_2);
+        }
 
-        self.adjacency_list
-            .get_mut(&key_2)
-            .map(|relations| relations.retain(|relation_key| *relation_key != key_1));
+        if let Some(relations) = self.adjacency_list.get_mut(&key_2) {
+            relations.retain(|relation_key| *relation_key != key_1);
+        }
     }
 
-    pub fn has(&self, value: &'a T) -> bool {
+    pub fn has(&self, value: &T) -> bool {
         self.adjacency_list.contains_key(&get_key(value))
     }
 
-    pub fn get_relations(&self, value: &'a T) -> Option<Vec<&T>> {
+    pub fn get_relations(&self, value: &T) -> Option<Vec<&T>> {
         match self.adjacency_list.get(&get_key(value)) {
             Some(relations) => relations
                 .iter()
@@ -103,6 +105,74 @@ where
                 .collect(),
             None => None,
         }
+    }
+
+    fn _get_relations(&self, key: u64) -> Option<&Vec<u64>> {
+        self.adjacency_list.get(&key)
+    }
+
+    pub fn can_traverse_to(&self, start: &T, finish: &T) -> bool {
+        if self.adjacency_list.is_empty() {
+            return false;
+        }
+
+        let start_key = get_key(start);
+        let target = get_key(finish);
+
+        let mut queue = deque![start_key];
+        let mut visited = HashSet::new();
+        visited.insert(start_key);
+
+        while let Some(node) = queue.pop_front() {
+            if let Some(relations) = self._get_relations(node) {
+                for key in relations {
+                    let key = *key;
+                    if key == target {
+                        return true;
+                    }
+                    if visited.contains(&key) {
+                        continue;
+                    }
+                    visited.insert(key);
+                    queue.push_back(key);
+                }
+            }
+        }
+        false
+    }
+
+    pub fn traverse_all_nodes(&self, start: &T) -> Vec<&T> {
+        let mut result = Vec::new();
+        if self.adjacency_list.is_empty() {
+            return result;
+        }
+
+        let start = get_key(start);
+        let mut visited = HashSet::new();
+
+        let mut stack = SinglyLinkedList::new();
+
+        // Add the start to the necessary structures
+        stack.push(start);
+
+        while let Some(next) = stack.pop() {
+            if visited.contains(&next) {
+                continue;
+            }
+
+            if let Some(relations) = self.adjacency_list.get(&next) {
+                relations
+                    .iter()
+                    .filter(|key| !visited.contains(*key))
+                    .for_each(|key| stack.push(*key));
+                visited.insert(next);
+            }
+
+            if let Some(node) = self.key_value_map.get(&next) {
+                result.push(node.value);
+            }
+        }
+        result
     }
 }
 
@@ -227,5 +297,56 @@ mod tests {
         assert_eq!(graph.get_relations(&str_2).unwrap().len(), 0);
         assert_eq!(graph.get_relations(&str_3).unwrap().len(), 0);
         assert_eq!(graph.get_relations(&str_4).unwrap().len(), 0);
+    }
+
+    #[test]
+    fn can_traverse_to() {
+        let mut graph = UndirectedGraph::default();
+
+        let str_1 = String::from("Key 1");
+        let str_2 = String::from("Key 2");
+        let str_3 = String::from("Key 3");
+        let str_4 = String::from("Key 4");
+
+        graph.add_vertex(&str_1);
+        graph.add_vertex(&str_2);
+        graph.add_vertex(&str_3);
+        graph.add_vertex(&str_4);
+
+        graph.add_edge(&str_1, &str_2);
+        graph.add_edge(&str_1, &str_3);
+        graph.add_edge(&str_2, &str_4);
+
+        assert!(graph.has(&str_1));
+        assert!(graph.has(&str_2));
+        assert!(graph.has(&str_3));
+        assert!(graph.has(&str_4));
+
+        assert!(graph.can_traverse_to(&str_1, &str_4));
+        graph.remove_vertex(&str_2);
+        assert!(!graph.can_traverse_to(&str_1, &str_4));
+    }
+
+    #[test]
+    fn traverse_all_nodes() {
+        let mut graph = UndirectedGraph::default();
+
+        let str_1 = String::from("Key 1");
+        let str_2 = String::from("Key 2");
+        let str_3 = String::from("Key 3");
+        let str_4 = String::from("Key 4");
+
+        graph.add_vertex(&str_1);
+        graph.add_vertex(&str_2);
+        graph.add_vertex(&str_3);
+        graph.add_vertex(&str_4);
+
+        graph.add_edge(&str_1, &str_2);
+        graph.add_edge(&str_1, &str_3);
+        graph.add_edge(&str_2, &str_4);
+
+        let result = graph.traverse_all_nodes(&str_1);
+        let expected = vec![&str_1, &str_3, &str_2, &str_4];
+        assert_eq!(result, expected);
     }
 }
